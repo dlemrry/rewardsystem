cc4e628433c7f58167a7211e3095881158a2b099
dlemrry <dg1411@naver.com>
2025-05-20 14:32:27 +0900

    [fix] fix configuration

--- libs/config/src/configuration.ts
+++ libs/config/src/configuration.ts
@@ -1,9 +1,11 @@
 export default () => ({
   database: {
-    port: process.env.PORT || 3000,
+    port: process.env.DB_PORT || 27017,
     username: process.env.DB_USER || 'admin',
     password: process.env.DB_PASSWORD || 'admin',
     dbName: process.env.DB_NAME,
   },
   jwtSecret: process.env.JWT_SECRET,
+  accessTokenExp: process.env.ACCESS_EXP || 100000,
+  refreshTokenExp: process.env.REFRESH_EXP || 1000000,
 });
